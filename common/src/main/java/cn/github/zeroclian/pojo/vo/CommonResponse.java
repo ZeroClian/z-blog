f75a6ccc56bbee65a1f90266c52de5145e29c1f4
ZeroClian <zeroycl@163.com>
2021-10-13 14:57:16 +0800

    add: 通用模块
    Signed-off-by: ZeroClian <zeroycl@163.com>

---
+++ common/src/main/java/cn/github/zeroclian/pojo/vo/CommonResponse.java
@@ -0,0 +1,35 @@
+package cn.github.zeroclian.pojo.vo;
+
+import cn.github.zeroclian.enumeration.CommonResultStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 统一响应对象定义
+ *
+ * @author ZeroClian
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResponse<T> implements Serializable {
+
+    private String code;
+    private String message;
+    private T data;
+
+    public CommonResponse(String code, String message) {
+
+        this.code = code;
+        this.message = message;
+    }
+
+    public CommonResponse(T data) {
+        this.data = data;
+        this.code = CommonResultStatus.SUCCESS.getCode();
+        this.message = CommonResultStatus.SUCCESS.getReason();
+    }
+}
