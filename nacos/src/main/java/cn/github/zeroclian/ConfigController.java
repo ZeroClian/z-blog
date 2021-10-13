31594c55cfd6414103a5a5c0e0f75dbbae814871
ZeroClian <zeroycl@163.com>
2021-10-13 14:12:03 +0800

    add：nacos注册中心

---
+++ nacos/src/main/java/cn/github/zeroclian/ConfigController.java
@@ -0,0 +1,28 @@
+package cn.github.zeroclian;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Desciption 配置控制器
+ * @Author: qiyiguo
+ * @Date: 2021-09-12 10:16 上午
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value(value = "${author.name}")
+    private String name;
+
+    @GetMapping("/get")
+    public String get() {
+        System.out.println(name);
+        return name;
+    }
+}
